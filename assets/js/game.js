290c980fec7850ddaa60b7f2c3bebc5270d5a716
EH <33641969+3mhurley@users.noreply.github.com>
2019-01-08 21:34:46 -0700

    more testing

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,34 +1,40 @@
 // Word List
 var words = ['apple','banana','kiwi','pear'];
 var guess = [];
-
+var goodGuess = [];
+var badGuess = [];
 var alpha = ['a','b','c'];
+var slWord;
+var slWordIndex;
 
 // Select Word
-var slWord;
+slWord = 'kiwi';
 
 // Convert Word to Array
 slWord = slWord.split('');
 
 document.onkeyup = function(event) {
     
-    if (slWordIndex === slWord.length()) {
+    if (slWordIndex === slWord.length) {
         return;
     }
     
     var keyInput = event.key.toLowerCase();
 
-    if (guess.includes(keyInput) === true) {
+    if (guess.includes(keyInput)) {
         // already guessed
         return;
     } else {
         // new guess
-        if (slWord.includes(keyInput) === true) {
+        if (slWord.includes(keyInput)) {
             // correct guess
-            guess.push(keyInput);
+            guess.push(keyInput); // adds key input to guess array
+            goodGuess.push(keyInput); // as letter hits we need to set the display of letter true
+            slWordIndex++
         } else {
             // incorrect guess
-            guess.push(keyInput);
+            guess.push(keyInput); // adds key input to guess array
+            badGuess.push(keyInput); // as letter hits we need to set the display of letter true
         }
     }
 
