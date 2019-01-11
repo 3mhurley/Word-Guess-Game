1329a56893d1ab6ab5b69ceb2d9775d0286c827c
EH <33641969+3mhurley@users.noreply.github.com>
2019-01-10 21:30:06 -0700

    objecting

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,7 +3,7 @@ var words = ['apple','banana','kiwi','pear'];
 var guess = [];
 var goodGuess = [];
 var badGuess = [];
-var alpha = ['a','b','c'];
+var alpha = [];
 var slWord;
 var slWordIndex;
 
@@ -12,6 +12,20 @@ slWord = 'kiwi';
 
 // Convert Word to Array
 slWord = slWord.split('');
+
+slWord.forEach(function(element) {
+    alpha.push('_');
+});
+
+slWord.forEach(function getAllIndexes(arr, val) {
+    var indexes = [], i;
+    for(i = 0; i < arr.length; i++)
+        if (arr[i] === val)
+            indexes.push(i);
+    return indexes;
+});
+
+console.log(indexes);
 
 document.onkeyup = function(event) {
     
@@ -37,9 +51,5 @@ document.onkeyup = function(event) {
             badGuess.push(keyInput); // as letter hits we need to set the display of letter true
         }
     }
-
-
-
-
 
 }
---
+++ assets/js/objGame.js
@@ -0,0 +1,19 @@
+var wordGuess = {
+    themeChoice: 'theme',
+    wordChoice: 'word',
+    guess: [],
+    correct: [],
+    incorrect: [],
+    theme: function(params) {
+        // Pick a theme
+    },
+    keyIn: function(params) {
+        // Key input
+    },
+    gGuess: function(params) {
+        // Code executed on a good guess
+    },
+    bGuess: function(params) {
+        // Code executed on a good guess
+    }
+}
